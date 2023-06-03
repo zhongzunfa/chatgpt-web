75cb4859433a739b2b5b040e9204ffed48cd7250
zhongzunfa <zhongzunfa@163.com>
2023-06-03 21:46:10 +0800

    add

--- src/router/permission.ts
+++ src/router/permission.ts
@@ -5,21 +5,23 @@ export function setupPageGuard(router: Router) {
   router.beforeEach(async (to, from, next) => {
     const authStore = useAuthStoreWithout()
     if (!authStore.session) {
-      try {
-        const data = await authStore.getSession()
-        if (String(data.auth) === 'false' && authStore.token)
-          authStore.removeToken()
-        if (to.path === '/500')
-          next({ name: 'Root' })
-        else
-          next()
-      }
-      catch (error) {
-        if (to.path !== '/500')
-          next({ name: '500' })
-        else
-          next()
-      }
+			next()
+
+      // try {
+      //   const data = await authStore.getSession()
+      //   if (String(data.auth) === 'false' && authStore.token)
+      //     authStore.removeToken()
+      //   if (to.path === '/500')
+      //     next({ name: 'Root' })
+      //   else
+      //     next()
+      // }
+      // catch (error) {
+      //   if (to.path !== '/500')
+      //     next({ name: '500' })
+      //   else
+      //     next()
+      // }
     }
     else {
       next()
